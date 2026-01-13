8646ac21895e1aad296ae46f00222208cdf1ed70
Jestsec <jestsec@gmail.com>
2026-01-12 21:09:33 -0500

    Fixing spelling errors in payload.sh comments

--- library/recon/access_point/continuous-ap-deauthentication/payload.sh
+++ library/recon/access_point/continuous-ap-deauthentication/payload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Title: Continous Deauth
-#Description: Payload to continuosly deauth all devices off of a network.
+#Title: Continuous Deauth
+#Description: Payload to continuously deauth all devices off of a network.
 # Author: Jestsec
 # Version: 1
 
